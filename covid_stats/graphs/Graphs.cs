acb9f472ebf6d0c6313770c740bc4b599fe1131a
zizwiz <zizwiz@engineer.com>
2021-06-09 20:53:37 +0100

    Added zoom factor to some graphs

--- covid_stats/graphs/Graphs.cs
+++ covid_stats/graphs/Graphs.cs
@@ -27,11 +27,32 @@ namespace covid_stats.graphs
             chrt_Daily.Legends.Clear(); // We do not need a legend
             chrt_Daily.ChartAreas[0].AxisX.IsMarginVisible = false;
             
+            Series series_daily = chrt_Daily.Series.Add("");
+            ChartArea ca_daily = chrt_Daily.ChartAreas[series_daily.ChartArea]; // get hold of chart
+            
+            // enable autoscroll
+            ca_daily.AxisX.ScaleView.Zoomable = true;
+            ca_daily.CursorX.AutoScroll = true;
+            ca_daily.CursorX.IsUserSelectionEnabled = true; //adds reset button on left
+            ca_daily.AxisX.ScaleView.SmallScrollSize = 100;
+            
 
             chrt_deaths.Series["Deaths"].ChartType = SeriesChartType.FastLine; //set type
             chrt_deaths.Series["Deaths"].Color = Color.Red; //set colour
             chrt_deaths.Legends.Clear(); // We do not need a legend
             chrt_deaths.ChartAreas[0].AxisX.IsMarginVisible = false;
+            
+            Series series_deaths = chrt_deaths.Series.Add("");
+            ChartArea ca_deaths = chrt_deaths.ChartAreas[series_deaths.ChartArea]; // get hold of chart
+            
+            // enable autoscroll
+            ca_deaths.AxisX.ScaleView.Zoomable = true;
+            ca_deaths.CursorX.AutoScroll = true;
+            ca_deaths.CursorX.IsUserSelectionEnabled = true; //adds reset button on left
+            ca_deaths.AxisX.ScaleView.SmallScrollSize = 100;
+
+            
+            
 
             chrt_cases_100k.Series["cases_100k"].ChartType = SeriesChartType.FastLine; //set type
             chrt_cases_100k.Series["cases_100k"].Color = Color.DarkOrange; //set colour
